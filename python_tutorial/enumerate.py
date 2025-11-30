e4773907a8dcd39ac481aae31f5da928d6a5d94b
tayyabkhan00 <iron29700@gmail.com>
2025-11-30 22:39:12 +0530

    methods

--- python_tutorial/enumerate.py
+++ python_tutorial/enumerate.py
@@ -2,3 +2,24 @@ fruits = ["apple", "banana", "mango"]
 
 for index, item in enumerate(fruits):
     print(index, item)
+
+fruits = ["apple", "banana", "mango"]
+
+# Starting index from 1
+for index, item in enumerate(fruits, start=1):
+    print(index, item)
+
+nums = [10, 20, 30]
+
+# Printing index and value of each element using enumerate
+for i, n in enumerate(nums):
+    print("Position:", i, "| Value:", n)
+
+nums = [5, 10, 15]
+
+# Doubling each element in the list using enumerate
+for i, n in enumerate(nums):
+    nums[i] = n * 2
+
+print(nums)
+
