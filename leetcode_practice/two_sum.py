2ef64d67296259d21ef19fc719c553586be8140a
tayyabkhan00 <iron29700@gmail.com>
2025-11-30 20:42:19 +0530

    using hash_map

--- leetcode_practice/two_sum.py
+++ leetcode_practice/two_sum.py
@@ -6,4 +6,12 @@ def twoSum(nums, target):
                 return [i, j]
 print(twoSum([2, 7, 11, 15], 9))
 
-
+# 2. Hash Map (Optimized Method — O(n))
+def twoSum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+print(twoSum([2, 7, 11, 15], 9))
