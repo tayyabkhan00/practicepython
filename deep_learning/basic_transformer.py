18ce787ecf0a536c3c059211bcfe1726c5a8f6b8
tayyabkhan00 <iron29700@gmail.com>
2025-12-11 02:17:34 +0530

    BERT Text Classification Project using HuggingFace + PyTorch.

--- deep_learning/basic_transformer.py
+++ deep_learning/basic_transformer.py
@@ -2,14 +2,15 @@ import torch
 import torch.nn as nn
 
 model = nn.Transformer(
-    d_model=512, 
-    nhead=8, 
+    d_model=512,
+    nhead=8,
     num_encoder_layers=6,
-    num_decoder_layers=6
+    num_decoder_layers=6,
+    batch_first=True
 )
 
-src = torch.rand((10, 32, 512))  # seq_len, batch, features
-tgt = torch.rand((20, 32, 512))
+src = torch.rand((32, 10, 512))  # batch, seq_len, features
+tgt = torch.rand((32, 20, 512))
 
 output = model(src, tgt)
 print(output.shape)
