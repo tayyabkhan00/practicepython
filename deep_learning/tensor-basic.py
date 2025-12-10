749641f8f78ee698610bc64f6f2408fb55fd68a0
tayyabkhan00 <iron29700@gmail.com>
2025-12-11 02:38:17 +0530

    tensor gradients

--- deep_learning/tensor-basic.py
+++ deep_learning/tensor-basic.py
@@ -18,3 +18,7 @@ x = torch.tensor([1, 2, 3]).to(device)
 print(x.device)
 
 # Tensor Gradients (Autograd)
+x = torch.tensor(2.0, requires_grad=True)
+y = x ** 3 + 2 * x
+y.backward()
+print(x.grad)   # derivative of y wrt x
